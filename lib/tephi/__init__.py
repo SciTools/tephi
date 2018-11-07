610bc8faad5b74bc7b98db4c86150f1829089e48
Bill Little <bill.james.little@gmail.com>
2018-11-07 23:43:24 +0000

    tidy setup and version

--- lib/tephi/__init__.py
+++ lib/tephi/__init__.py
@@ -39,7 +39,7 @@ from . import isopleths
 from . import transforms
 
 
-__version__ = '0.2.0-alpha'
+__version__ = '0.2.0.dev0'
 
 
 #
--- setup.py
+++ setup.py
@@ -1,24 +1,51 @@
-from setuptools import setup
+from __future__ import absolute_import, division, print_function
+
+import os
+from setuptools import find_packages, setup
 
 
-setup(
-    name='tephi',
-    version='0.2.0-alpha',
-    url='https://github.com/SciTools/tephi',
-    author='Bill Little',
-    author_email='bill.james.little@gmail.com',
-    packages=['tephi', 'tephi.tests'],
+NAME = 'tephi'
+DIR = os.path.abspath(os.path.dirname(__file__))
+BASE = os.path.join(DIR, 'lib')
+
+
+def extract_version():
+    version = None
+    fname = os.path.join(BASE, NAME, '__init__.py')
+    with open(fname, 'r') as fi:
+        for line in fi:
+            if (line.startswith('__version__')):
+                _, version = line.split('=')
+                version = version.strip()[1:-1]  # Remove quotation characters
+                break
+    return version
+
+
+def long_description():
+    with open(os.path.join(DIR, 'README.rst'), 'r') as fi:
+        long_description = ''.join(fi.readlines())
+    return long_description
+
+
+args = dict(
+    name=NAME,
+    version=extract_version(),
+    author='UK Met Office',
+    packages=find_packages(where=BASE),
     package_dir={'': 'lib'},
-    package_data={'tephi': ['etc/test_data/*.txt'] + ['etc/test_results/*.pkl']},
+    package_data={'tephi': ['etc/test_data/*.txt',
+                            'etc/test_results/*.pkl']},
     classifiers=['License :: OSI Approved :: '
-                 'GNU Lesser General Public License v3 (LGPLv3)',
+                 'GNU Lesser General Public License v3 or later (LGPLv3+)',
                  'Programming Language :: Python',
                  'Programming Language :: Python :: 2',
-                 'Programming Language :: Python :: 2.7',
-                 'Programming Language :: Python :: 3',
-                 'Programming Language :: Python :: 3.3',
-                 'Programming Language :: Python :: 3.4',],
+                 'Programming Language :: Python :: 2.7'],
     description='Tephigram plotting in Python',
-    long_description=open('README.rst').read(),
-    test_suite='tephi.tests',
+    long_description=long_description(),
+    long_description_content_type='text/x-rst',
+    test_suite='{}.tests'.format(NAME),
 )
+
+
+if __name__ == '__main__':
+    setup(**args)
